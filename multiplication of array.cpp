e51067f1b92d9da6e0af1664e4e41627f3a87a2a
Shivam Shriwastav <139604077+shivam808047@users.noreply.github.com>
2024-09-06 22:11:41 +0530

    Add files via upload

---
+++ multiplication of array.cpp
@@ -0,0 +1,22 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int multiply(int array[], int n)
+{
+	int pro = 1;
+	for (int i = 0; i < n; i++) 
+		pro = pro * array[i];
+	return pro;
+}
+
+
+int main()
+{
+	int array[] = {1, 2, 3, 4, 5, 6};
+	int n = sizeof(array) / sizeof(array[0]);
+	
+	cout << multiply(array, n);
+	return 0;
+}
