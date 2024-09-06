8c02a1a04f6a850f4f502c228f419ac97064fd73
Shivam Shriwastav <139604077+shivam808047@users.noreply.github.com>
2024-09-06 22:43:25 +0530

    Add files via upload

---
+++ insertion in array.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int insertSorted(int arr[], int n, int key, int capacity) { 
+
+  if (n >= capacity) 
+    return n; 
+
+  arr[n] = key; 
+
+    return (n + 1); 
+} 
+int main() {
+  int arr[20] = {12, 16, 20, 40, 50, 70}; 
+  
+  int capacity = sizeof(arr) / sizeof(arr[0]);
+  
+  int n = 6; 
+  int i, key = 26;
+  
+  cout << "\n Before Insertion: "; 
+  for (i = 0; i < n; i++) 
+    cout << arr[i] << " "; 
+  
+  n = insertSorted(arr, n, key, capacity);
+  
+  cout << "\n After Insertion: "; 
+  for (i = 0; i < n; i++) 
+    cout << arr[i] << " "; 
+  
+  return 0;
+
+}
